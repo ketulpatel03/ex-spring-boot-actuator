aa127abf854888e16c31ad348fb106dd349544a3
ketulpatel03 <ketulpatel03@yahoo.co.in>
2024-04-20 18:39:28 -0600

    first commit

---
+++ src/main/java/com/java/ExSpringBootActuatorApplication.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExSpringBootActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExSpringBootActuatorApplication.class, args);
+    }
+
+}
